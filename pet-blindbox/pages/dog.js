ca9d41204669a0cf7263ba7fa1ec6e871588171f
Michelle-Linnn <wlin13@inside.artcenter.edu>
2026-03-18 17:49:41 -0700

    12

--- pet-blindbox/pages/dog.js
+++ pet-blindbox/pages/dog.js
@@ -1,6 +1,8 @@
 import React from 'react';
+// 导入我们之前写好的通用订阅页面组件
 import SubscriptionPage from '../components/SubscriptionPage';
 
+// 🌟 核心：必须使用 export default，这样 Next.js 才能识别这个页面
 export default function DogPage() {
   return <SubscriptionPage type="dog" />;
 }
